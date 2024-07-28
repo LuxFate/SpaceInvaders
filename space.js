9012b357be5427c59d438fed7e32b81e0691db9b
LuxFate <143355319+LuxFate@users.noreply.github.com>
2024-07-28 18:33:05 +0200

    Changes made

--- space.js
+++ space.js
@@ -27,12 +27,36 @@ let shipVelocityX = tileSize;
 window.onload = function(){
     map = document.getElementById("map");
     map.width = mapWidth;
-    map.Height = mapHeight;
+    map.height = mapHeight;
     context = map.getContext("2d")
 
     shipImg = new Image();
     shipImg.src = "./ship.png";
     shipImg.onload = function(){
-        context.drawImage()
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+    }
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+}
+
+function update(){
+    requestAnimationFrame(update);
+
+    context.clearRect(0, 0, map.width, map.height);
+ 
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+}
+
+function moveShip(e){
+    //checks if left key is pressed and if the x position 
+    //of ship reaches the left side of the canvas
+    if(e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0){
+        ship.x -= shipVelocityX;
+    }
+    //checks if right key is pressed and if the x position 
+    //of ship reaches the right side of the canvas
+    else if(e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= map.width){
+        ship.x += shipVelocityX;
     }
 }
